d079f6c03b1639be2319497db757179515aafeaa
abubatjargal <abu@macbook-pro.myfiosgateway.com>
2020-09-24 17:23:24 -0400

    Updates

---
+++ precache-manifest.adc92d64ce6b7785564c89fbdb9ae0e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e561b78525354925bb3674efb9ec2e9e",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "7565010147f3c5f503f2",
+    "url": "/my-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "982f8db5562c50b0d968",
+    "url": "/my-portfolio/static/js/2.c7663357.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.c7663357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7565010147f3c5f503f2",
+    "url": "/my-portfolio/static/js/main.cbd17a86.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "a17448661b35742946d92090308dac42",
+    "url": "/my-portfolio/static/media/profile-image.a1744866.jpg"
+  }
+]);
